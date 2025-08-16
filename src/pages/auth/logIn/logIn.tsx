05f52c22adc0252903ace8db78b821e39e920e14
cvrse <schizoflow@proton.me>
2025-08-17 01:04:59 +0300

    form has been created, i will refactor it.

--- src/pages/auth/logIn/logIn.tsx
+++ src/pages/auth/logIn/logIn.tsx
@@ -1,6 +1,8 @@
 import type { FC } from "react";
-import { Button } from "@/shared/ui/button";
+// import { Button } from "@/shared/ui/button";
 
 export const LogIn: FC = () => {
-  return <Button size="large" variant="sign">Sign In</Button>;
+  return (
+    <div></div>
+  )
 };
---
+++ src/shared/ui/form/formWrapper.tsx
@@ -0,0 +1,22 @@
+// import type { FC, ReactNode, FormHTMLAttributes, } from 'react';
+// import styles from "./formWrapper.scss"
+//
+// interface FormWrapperProps extends FormHTMLAttributes<HTMLFormElement> {
+//   submitText: string;
+//   children?: ReactNode;
+// }
+//
+// export const FormWrapper: FC<FormWrapperProps> = ({
+//   submitText,
+//   children,
+//   ...props
+// }) => {
+//   return (
+//     <form className={styles.form} {...props}>
+//       <div className={styles.fields}>{children}</div>
+//       <button type='submit' className={submit}>
+//         {submitText}
+//       </button>
+//     </form>
+//   )
+// }
