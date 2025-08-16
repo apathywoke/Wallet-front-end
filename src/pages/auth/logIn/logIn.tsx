a8b33207c0f741fa6d3871a4cfd07789f3e086f8
cvrse <asdysphoria@gmail.com>
2025-08-17 00:24:12 +0300

    button.module.scss has been changed.

--- src/pages/auth/logIn/logIn.tsx
+++ src/pages/auth/logIn/logIn.tsx
@@ -2,5 +2,5 @@ import type { FC } from "react";
 import { Button } from "@/shared/ui/button";
 
 export const LogIn: FC = () => {
-  return <Button size="small">Войти</Button>;
+  return <Button size="large" variant="sign">Sign In</Button>;
 };
