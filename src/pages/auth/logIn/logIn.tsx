3a45a978a9c6513effed67a832b2446353ae2499
cvrse <schizoflow@proton.me>
2025-08-18 13:51:35 +0300

    Shared changes. Added: Button, Form, Input.

--- src/pages/auth/logIn/logIn.tsx
+++ src/pages/auth/logIn/logIn.tsx
@@ -1,8 +1,16 @@
 import type { FC } from "react";
-// import { Button } from "@/shared/ui/button";
+import styles from "./logIn.module.scss";
+import { Button } from "@/shared/ui/button";
+import { FormWrapper} from '@/shared/ui/form';
+import { InputField} from '@/shared/ui/input';
 
 export const LogIn: FC = () => {
   return (
-    <div></div>
+    <main className={styles.mainClass}>
+    <FormWrapper>
+      <InputField placeHolder={"WriteSmth"} type={"password"}></InputField>
+      <Button variant={"sign"} size={"large"}>Sign In</Button>
+    </FormWrapper>
+    </main>
   )
 };
--- src/shared/ui/form/formWrapper.tsx
+++ src/shared/ui/form/formWrapper.tsx
@@ -1,22 +1,17 @@
-// import type { FC, ReactNode, FormHTMLAttributes, } from 'react';
-// import styles from "./formWrapper.scss"
-//
-// interface FormWrapperProps extends FormHTMLAttributes<HTMLFormElement> {
-//   submitText: string;
-//   children?: ReactNode;
-// }
-//
-// export const FormWrapper: FC<FormWrapperProps> = ({
-//   submitText,
-//   children,
-//   ...props
-// }) => {
-//   return (
-//     <form className={styles.form} {...props}>
-//       <div className={styles.fields}>{children}</div>
-//       <button type='submit' className={submit}>
-//         {submitText}
-//       </button>
-//     </form>
-//   )
-// }/
+import type { FC, ReactNode, FormHTMLAttributes, } from 'react';
+import styles from "./formWrapper.module.scss"
+
+interface FormWrapperProps extends FormHTMLAttributes<HTMLFormElement> {
+  children?: ReactNode;
+}
+
+export const FormWrapper: FC<FormWrapperProps> = ({
+  children,
+  ...props
+}) => {
+  return (
+    <form className={styles.form} {...props}>
+      <div className={styles.fields}>{children}</div>
+    </form>
+  )
+}
